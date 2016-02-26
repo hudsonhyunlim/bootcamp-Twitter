55cedfd29d6629152533d95ce194e32ed0cb3d3f
Hyun Lim <hlim@lyft.com>
2016-02-25 21:41:21 -0800

    Create base profile page

--- Bootcamp-Twitter/User.swift
+++ Bootcamp-Twitter/User.swift
@@ -13,8 +13,12 @@ public class User: NSObject {
     var name: String?
     var screenName: String?
     var profileImageUrl: NSURL?
+    var backgroundImageUrl: NSURL?
     var location: String?
     var retweetCount: Int = 0
+    var followingCount: Int = 0
+    var followerCount: Int = 0
+    var statusesCount: Int = 0
     var userDescription: String?
     var dictionary: NSDictionary?
     
@@ -24,10 +28,17 @@ public class User: NSObject {
         self.screenName = dictionary["screen_name"] as? String
         self.location = dictionary["location"] as? String
         self.retweetCount = dictionary["retweet_count"] as? Int ?? 0
+        self.followerCount = dictionary["followers_count"] as? Int ?? 0
+        self.followingCount = dictionary["friends_count"] as? Int ?? 0
+        self.statusesCount = dictionary["statuses_count"] as? Int ?? 0
         self.userDescription = dictionary["description"] as? String
         
         if let url = dictionary["profile_image_url_https"] as? String {
             self.profileImageUrl = NSURL(string: url)
+        }
+        
+        if let url = dictionary["profile_background_image_url_https"] as? String {
+            self.backgroundImageUrl = NSURL(string: url)
         }
     }
     
--- Bootcamp-Twitter/UserProfileViewController.swift
+++ Bootcamp-Twitter/UserProfileViewController.swift
@@ -11,13 +11,32 @@ import UIKit
 class UserProfileViewController: UIViewController {
 
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var screenNameLabel: UILabel!
+    @IBOutlet weak var userDescriptionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var followingCountLabel: UILabel!
+    @IBOutlet weak var followerCountLabel: UILabel!
+    @IBOutlet weak var tweetCountLabel: UILabel!
     
     var user:User? {
         didSet {
             if let user = self.user {
+                self.view.layoutIfNeeded()
                 if let imageUrl = user.profileImageUrl {
                     self.profileImageView.setImageWithURL(imageUrl)
                 }
+                if let backgroundUrl = user.backgroundImageUrl {
+                    self.backgroundImageView.setImageWithURL(backgroundUrl)
+                }
+                self.userNameLabel.text = user.name
+                self.screenNameLabel.text = user.screenName
+                self.userDescriptionLabel.text = user.userDescription
+                self.locationLabel.text = user.location
+                self.followingCountLabel.text = String(user.followingCount)
+                self.followerCountLabel.text = String(user.followerCount)
+                self.tweetCountLabel.text = String(user.statusesCount)
             }
         }
     }
